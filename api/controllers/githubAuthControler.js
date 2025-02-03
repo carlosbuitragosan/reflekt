0166bef0fa8264744105286f9b184db785c19c53
Carlos Buitrago <letscatchupagain@gmail.com>
2025-02-03 16:13:07 +0000

    Refactor google and github callback handlers to use explicit error handling.

--- api/controllers/githubAuthControler.js
+++ api/controllers/githubAuthControler.js
@@ -5,19 +5,21 @@ export const githubSignInHandler = passport.authenticate('github', {
   scope: ['user:email'],
 });
 
-// Github sign in callback handler
+// Github sign in callback handler using passport.authenticate custom callback (err, user, info) which helps logging errors
 export const githubSignInCallbackHandler = (req, res, next) => {
-  passport.authenticate('github', {
-    session: false,
-    failureRedirect: '/login',
-  })(req, res, () => {
-    console.log('user from callback: ', req.user);
-    if (req.user) {
-      console.log('Github user authenticated: ', req.user);
-      return res.redirect(
-        `http://localhost:5173/login?email=${req.user.email}&githubId=${req.user.githubId}`,
-      );
+  passport.authenticate('github', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('Error during GitHub authentication: ', err);
+      return res.status(500).json({ error: 'Internal server error.' });
     }
-    return res.status(401).json({ error: 'GitHub authentication failed.' });
-  });
+    if (!user) {
+      console.log('GitHub authentication failed: ', info);
+      return res.redirect('/login');
+    }
+    req.user = user;
+
+    return res.redirect(
+      `http://localhost:5173/login?email=${req.user.email}&githubId=${req.user.githubId}`,
+    );
+  })(req, res, next); // <-- call passport middleware
 };
--- api/controllers/googleAuthController.js
+++ api/controllers/googleAuthController.js
@@ -4,17 +4,34 @@ export const googleSignInHandler = passport.authenticate('google', {
   scope: ['email', 'profile'],
 });
 
-export const googleSignInCallbackHandler = (req, res) => {
-  passport.authenticate('google', {
-    session: false,
-    failureRedirect: '/login',
-  })(req, res, () => {
-    if (req.user) {
-      console.log('user authenticated: ', req.user);
-      return res.redirect(
-        `http://localhost:5173/login?email=${req.user.email}&googleId=${req.user.googleId}`,
-      );
+export const googleSignInCallbackHandler = (req, res, next) => {
+  passport.authenticate('google', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('Error during Google authentication: ', err);
+      return res.status(500).json({ error: 'Internal server error.' });
     }
-    return res.status(401).json({ error: 'Google authentication failed' });
-  });
+    if (!user) {
+      console.log('Google authentication failed: ', info);
+      return res.redirect('/login');
+    }
+    req.user = user;
+
+    return res.redirect(
+      `http://localhost:5173/login?email=${req.user.email}&googleId=${req.user.googleId}`,
+    );
+  })(req, res, next);
 };
+// export const googleSignInCallbackHandler = (req, res) => {
+//   passport.authenticate('google', {
+//     session: false,
+//     failureRedirect: '/login',
+//   })(req, res, () => {
+//     if (req.user) {
+//       console.log('user authenticated: ', req.user);
+//       return res.redirect(
+//         `http://localhost:5173/login?email=${req.user.email}&googleId=${req.user.googleId}`,
+//       );
+//     }
+//     return res.status(401).json({ error: 'Google authentication failed' });
+//   });
+// };
